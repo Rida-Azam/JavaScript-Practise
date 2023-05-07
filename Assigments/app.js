24a89524639815b14adeaa47e5eeff6147e24503
Rida <19103100.rida@gmail.com>
2023-05-07 10:55:56 -0700

    Alert Assignment

---
+++ Assigments/app.js
@@ -0,0 +1,12 @@
+//  Chapter no 1 Alert Assignments
+
+// alert("Welcome to Javascript Class!")
+
+// alert("Error! Please enter a valid password")
+
+// alert("Welcome to JS Land... \nHappy Coding!")
+
+
+
+
+
---
+++ Script/app.js
@@ -0,0 +1,6 @@
+// 3 May 2023 Practise 
+
+// var numberOfStars = Math.round(scoreAvg);
+// document.write(numberOfStars)
+// alert("Welcome to Javascript Class!")
+// document.write(alert)
