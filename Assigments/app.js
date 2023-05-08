84c245f97f5ab37ee54fe520e0c86b3abf33965f
Rida <19103100.rida@gmail.com>
2023-05-08 13:43:49 -0700

    practice math function

--- Assigments/app.js
+++ Assigments/app.js
@@ -8,5 +8,29 @@
 
 
 
+//chap 2 variable assignment
+
+
+// var username;
+
+// var myName="Rida Azam";
+
+
+// var message="Hello World"
+// alert(message)
+
+// var bio="Rida Azam\nI am 20 year old\nFrontend Development"
+// alert(bio)
+
+// var piz="PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(piz)
+
+// var imo="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(imo)
+
+
+// document.write("my name is rida azam!!")
+
+//chapter 3 variable for number
 
 
--- Script/app.js
+++ Script/app.js
@@ -4,3 +4,24 @@
 // document.write(numberOfStars)
 // alert("Welcome to Javascript Class!")
 // document.write(alert)
+
+// var scoreAvg = Math.ceil(.000001);
+// console.log(scoreAvg);
+
+
+// var scoreAvg = Math.sin(90);
+// console.log(scoreAvg);
+
+
+// var scoreAvg = Math.random();
+// console.log(scoreAvg);
+
+// var scoreAvg = Math.cos(90);
+// console.log(scoreAvg);
+
+
+
+// 8 may 2023 
+
+//make a function that calculate your age
+
